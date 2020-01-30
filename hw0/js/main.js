1c6f419ae4131fb53e63ccffb83523767246e89e
apex256 <davidmcm979@gmail.com>
2020-01-30 10:09:58 -0500

    SSH Test 1

--- hw0/js/main.js
+++ hw0/js/main.js
@@ -7,7 +7,7 @@ function make_main_game_state( game )
         game.load.image( 'logo', 'assets/phaser.png' );
     }
     
-    var bouncy;
+    let bouncy;
     
     function create() {
         // Create a sprite at the center of the screen using the 'logo' image.
@@ -23,8 +23,8 @@ function make_main_game_state( game )
         
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
+        let style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
+        let text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
         text.anchor.setTo( 0.5, 0.0 );
     }
     
@@ -54,7 +54,7 @@ window.onload = function() {
     // loading functions to reflect where you are putting the assets.
     // All loading functions will typically all be found inside "preload()".
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game' );
+    let game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game' );
     
     game.state.add( "main", make_main_game_state( game ) );
     
