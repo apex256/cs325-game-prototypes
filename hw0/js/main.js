b57fa6397a0c9262bb97f26f9f48c9e9ff104489
David McMasters <davidmcmasters@Davids-MBP.fios-router.home>
2020-02-08 15:55:09 -0500

    Added menu sounds/music and cleaned up some code

--- hw0/js/main.js
+++ hw0/js/main.js
@@ -32,37 +32,4 @@ game.state.start('menu');
     
     game.state.start( "main" );
 };
-
-function make_main_game_state( game )
-{
-    function preload() {
-        // Load an images and name them as such
-        this.load.image('sky', 'assets/sky.png');
-        this.load.image('ground', 'assets/platform.png');
-        this.load.image('star', 'assets/star.png');
-        this.load.image('bomb', 'assets/bomb.png');
-        this.load.spritesheet('dude', 'assets/dude.png',
-            {frameWidth: 32, frameHeight: 48});
-    }
-    
-    let platforms;
-    
-    function create() {
-        this.add.image(0, 0, 'sky');
-        
-        platforms = this.physics.add.staticGroup();
-
-        platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-        platforms.create(600, 400, 'ground');
-        platforms.create(50, 250, 'ground');
-        platforms.create(750, 220, 'ground');
-    }
-    
-    function update() {
-        
-    }
-    
-    return { "preload": preload, "create": create, "update": update };
-}
 */
--- hw0/js/states/menu.js
+++ hw0/js/states/menu.js
@@ -5,12 +5,16 @@ myGame.Menu = function(game) {
     let copyright;
     let startTextTimer;
     let enterKey;
+    let music;
+    let buttonSound;
 };
 
 myGame.Menu.prototype = {
     preload: function() {
         this.load.bitmapFont('myFont', 'assets/fonts/bitmapFonts/font.png', 'assets/fonts/bitmapFonts/font.fnt');
         this.load.image('sky', 'assets/skies/sky2.png');
+        this.load.audio('music01', 'assets/audio/music01.ogg');
+        this.load.audio('pressEnter', 'assets/audio/ui_press.ogg')
     },
 
     create: function () {
@@ -19,6 +23,8 @@ myGame.Menu.prototype = {
         startTextTimer = 0;
         startText = this.add.bitmapText((this.world.width/2)-240, (this.world.height/2)-16, 'myFont', 'PRESS ENTER TO BEGIN', 32);
         copyright = this.add.bitmapText(6, this.world.height-24, 'myFont', 'Made by David Mark McMasters', 16);
+        music = this.add.audio('music01');
+        buttonSound = this.add.audio('pressEnter');
 
         // Inputs
         enterKey = this.input.keyboard.addKey(Phaser.Keyboard.ENTER);
@@ -38,5 +44,7 @@ myGame.Menu.prototype = {
 
     startGame: function() {
         this.state.start('level01');
+        buttonSound.play();
+        music.play();
     }
 }
